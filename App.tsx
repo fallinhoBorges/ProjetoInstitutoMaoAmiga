b888f059bce46d6c3a1b9b56536b94b39394090c
Caio Cintra B. <caioexecutivo11@gmail.com>
2026-08-09 14:15:31 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+type Produto = (
+  id: string,
+  nome: string,
+  preco: number,
+  imagem: string
+)
+
+const produtos: Produto[] = [
+  (id: 1, nome: 'Produto1', preco: 25.50, imagem: 'wwwwww'),
+  (id: 2, nome: 'Produto2', preco: 40.00, imagem: 'wwwweee'),
+];
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Bem-vindo!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
